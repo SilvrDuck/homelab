3cc5d2bc99df9b68d99b12d1c35cae6a9d349ded
Thibault <thibault@thvi.ch>
2024-10-05 21:03:15 +0000

    enhance setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 pipx install pre-commit
 pre-commit install
 docker network create web
+mkdir -p ./data/traefik/acme
+touch ./data/traefik/acme/acme.json
+echo "Need to chmod 600 ./data/traefik/acme/acme.json"
+
